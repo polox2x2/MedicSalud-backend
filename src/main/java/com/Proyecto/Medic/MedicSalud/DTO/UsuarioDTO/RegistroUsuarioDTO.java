b955ea0fedaa692a5a3f7afe945218af4096206f
polox2x2 <nekdek1450@gmail.com>
2025-09-26 17:24:34 -0500

    Se creo RegisterDTO con sus atributos

---
+++ src/main/java/com/Proyecto/Medic/MedicSalud/DTO/UsuarioDTO/RegistroUsuarioDTO.java
@@ -0,0 +1,23 @@
+package com.Proyecto.Medic.MedicSalud.DTO.UsuarioDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegistroUsuarioDTO {
+    String nombre ;
+    String apellido;
+    Integer dni;
+    String correo;
+    String clave;
+    String telefono;
+    String direccion;
+    LocalDateTime fechanaciemiento;
+}
