f4d909ebd5283d8f7ae49e568363a3e4934b061e
polox2x2 <nekdek1450@gmail.com>
2025-09-26 17:59:12 -0500

    Creacion de metodo para el controller , CrearUsuarioConPaciente

--- src/main/java/com/Proyecto/Medic/MedicSalud/Controller/UsuarioController.java
+++ src/main/java/com/Proyecto/Medic/MedicSalud/Controller/UsuarioController.java
@@ -1,13 +1,17 @@
 package com.Proyecto.Medic.MedicSalud.Controller;
 
 
+import com.Proyecto.Medic.MedicSalud.DTO.UsuarioDTO.RegistroUsuarioDTO;
 import com.Proyecto.Medic.MedicSalud.DTO.UsuarioDTO.UsuarioDTO;
+import com.Proyecto.Medic.MedicSalud.Entity.Usuario;
 import com.Proyecto.Medic.MedicSalud.Service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/usuarios")
@@ -30,6 +34,19 @@ public class UsuarioController {
     public ResponseEntity<UsuarioDTO> crear(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.crear(usuarioDTO));
     }
+    @PostMapping("/crear")
+    public ResponseEntity<?>crearUsuaroConPaciente (@RequestBody RegistroUsuarioDTO dto){
+        Usuario usuario = usuarioService.registrarUsuarioComoPaciente(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "id",usuario.getId(),
+                "email",usuario.getEmail(),
+                "roles",usuario.getRoles(),
+                "pacienteCreado",true
+
+        ));
+    }
+
+
 
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDTO> actualizar(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
