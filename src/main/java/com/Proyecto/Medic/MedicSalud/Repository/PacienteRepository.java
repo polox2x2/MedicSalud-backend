8dd0bac88f1e61649889908ba0b21eea48f75fab
polox2x2 <nekdek1450@gmail.com>
2025-09-26 17:11:32 -0500

    Se agregaron 3 metodos en PacienteRepository , existsByUsuario,existsByEmaily existsByDni

--- src/main/java/com/Proyecto/Medic/MedicSalud/Repository/PacienteRepository.java
+++ src/main/java/com/Proyecto/Medic/MedicSalud/Repository/PacienteRepository.java
@@ -1,9 +1,15 @@
 package com.Proyecto.Medic.MedicSalud.Repository;
 
 import com.Proyecto.Medic.MedicSalud.Entity.Paciente;
+import com.Proyecto.Medic.MedicSalud.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+    boolean existsByUsuario (Usuario usuario);
+    boolean existsByEmail(String emain);
+    boolean existsByDni (Integer dni);
+
+
 }
